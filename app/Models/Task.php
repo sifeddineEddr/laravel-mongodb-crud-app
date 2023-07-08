0454beaa0dfdad348cb03ac529a74ef3692f5a1f
sifeddineeddr <sifeddine.edr@gmail.com>
2023-07-08 16:25:36 +0100

    migrations, factories and seeder

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,22 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
-class User extends Model
+class Task extends Model
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'first_name',
         'last_name',
     ];
 
-    public function tasks()
+    public function owner()
     {
-        return $this->hasMany(Task::class);
+        return $this->belongsTo(User::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(4)->create();
+        \App\Models\Task::factory(15)->create();
     }
 }
